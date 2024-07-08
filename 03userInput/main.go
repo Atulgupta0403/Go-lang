ee7d8d8eaabb61e66373dee888225f51f2f7e526
atulgupta0403 <atulgupta0403@gmail.com>
2024-07-08 21:31:54 +0530

    userInput

---
+++ 03userInput/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("enter any string")
+
+	// comma ok / err err
+	input , _ := reader.ReadString('\n')
+	fmt.Println("thanks to write " , input)
+}
